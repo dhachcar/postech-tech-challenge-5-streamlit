9f7d6461592ab0acadd81c157559c9cb93c56c88
Danilo Henrique Achcar <dnl_pnd16@hotmail.com>
2024-07-20 22:56:30 -0300

    feat: finalizado redação das instituicoes de ensino

--- tabs/analise/demografico/demografico_instituicao_tab.py
+++ tabs/analise/demografico/demografico_instituicao_tab.py
@@ -29,8 +29,7 @@ class AnaliseDemograficoInstituicaoTab(TabInterface):
                 xaxis="Instituição de ensino",
             )
             st.plotly_chart(fig, use_container_width=True)
-
-            st.markdown("TODO: redigir... falar da maioria esmagadora ter vindo/ou estudar em escola publica e mostrar uma tabela com alunos que estao na FIAP")
+            st.markdown("Conforme podemos observar no gráfico acima, a maioria esmagadora dos estudantes atendidos pela ONG são oriundos do ensino público. Isto apenas confirma o papel de destaque que a **:blue[Passos Mágicos]** exerce na vida dessas pessoas, que na maioria dos casos, não possui um ensino de qualidade ou incentivo do estado. Inclusive, parte dos alunos atendidos pela ONG também ingressaram na **:blue[FIAP]** ou outras instituições de ensino superior, reforçando o papel transformador exercido pela organização.")
 
             st.subheader(f":blue[2021]", divider="blue")
             fig = plot_bar(
@@ -40,11 +39,9 @@ class AnaliseDemograficoInstituicaoTab(TabInterface):
                 xaxis="Instituição de ensino",
             )
             st.plotly_chart(fig, use_container_width=True)
-
-            with st.expander(":blue[Comentários]", expanded=True):
-                st.markdown("TODO: redigir")
+            st.markdown("Da mesma forma que no ano anterior, podemos observar que boa parte das crianças/jovens atendidos pela ONG são oriundos da rede pública de ensino.")
 
             st.error(
-                "**Não há dados disponiveis a respeito das instituições de ensino em 2022 na Passos Mágicos**",
+                "**IMPORTANTE**: Não há dados disponiveis a respeito das instituições de ensino em 2022 na Passos Mágico",
                 icon=":material/help:",
             )
